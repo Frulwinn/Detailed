db5f12bd8924c09334f21fd2def5ee87ad22f7e1
Frulwinn <frulwinn@gmail.com>
2018-11-27 14:16:27 -0800

    initial commit

--- Detailed/Detailed/DetailViewController.swift
+++ Detailed/Detailed/DetailViewController.swift
@@ -2,23 +2,4 @@ import UIKit
 
 class DetailViewController: UIViewController {
     
-    var character: Model.Animation?
-    
-    @IBOutlet weak var label: UILabel!
-    @IBOutlet weak var imageBig: UIImageView!
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        guard let character = character else { return }
-        
-        let cells = Model.shared.cells(for: character)
-        imageBig.animationImages = cells
-        imageBig.animationRepeatCount = 0
-        imageBig.animationDuration = 2 // FIGURE THIS PART OUT
-        imageBig.startAnimating()
-        
-        // Set the label
-        label.text = character.rawValue
-    }
 }
--- Detailed/Detailed/TableViewCell.swift
+++ Detailed/Detailed/TableViewCell.swift
@@ -1,7 +1,5 @@
 import UIKit
 
 class TableViewCell: UITableViewCell{
-    
-    @IBOutlet weak var imageAnimation: UIImageView!
-    
+   
 }
